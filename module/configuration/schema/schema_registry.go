4bdbef7d283f7f697eb71e75540d25a505eff955
bobyindra <bobyindra@outlook.com>
2025-11-10 11:48:41 +0700

    add more schema configs

--- module/configuration/schema/schema_registry.go
+++ module/configuration/schema/schema_registry.go
@@ -9,6 +9,10 @@ import (
 const (
 	PAYMENT_CONFIG = "payment-config"
 	FEE_CONFIG     = "fee-config"
+	BANNER_CONFIG  = "banner-config"
+	BCA_ENABLED    = "bca-enabled"
+	EMAIL_CONFIG   = "email-config"
+	WORDING_CONFIG = "wording-config"
 )
 
 func GetSchemaByConfigName(cfgName string) ([]byte, error) {
@@ -17,6 +21,14 @@ func GetSchemaByConfigName(cfgName string) ([]byte, error) {
 		return os.ReadFile("./module/configuration/schema/payment_config.json")
 	case FEE_CONFIG:
 		return os.ReadFile("./module/configuration/schema/fee_config.json")
+	case BANNER_CONFIG:
+		return os.ReadFile("./module/configuration/schema/banner_config.json")
+	case BCA_ENABLED:
+		return os.ReadFile("./module/configuration/schema/bca_enabled.json")
+	case EMAIL_CONFIG:
+		return os.ReadFile("./module/configuration/schema/email_config.json")
+	case WORDING_CONFIG:
+		return os.ReadFile("./module/configuration/schema/wording_config.json")
 	default:
 		return nil, entity.ErrConfigNotFound
 	}
