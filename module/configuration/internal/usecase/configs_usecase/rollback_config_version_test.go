c7363d18846b4a73ed4951fac519d62191c43475
bobyindra <bobyindra@outlook.com>
2025-11-13 08:56:05 +0700

    addtest: rollback config version usecase

---
+++ module/configuration/internal/usecase/configs_usecase/rollback_config_version_test.go
@@ -0,0 +1,146 @@
+package configsusecase_test
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/bobyindra/configs-management-service/internal/testutil"
+	"github.com/bobyindra/configs-management-service/module/configuration/entity"
+	"github.com/golang/mock/gomock"
+)
+
+func (s *configsUsecaseSuite) TestConfigs_RollbackConfig_Success() {
+	s.Run("Rollback Config - Success", func() {
+		// Given
+		ctx := context.TODO()
+		config := &entity.Config{
+			Name:    "test",
+			Version: 1,
+		}
+		getParam := &entity.GetConfigRequest{
+			Name: config.Name,
+		}
+		cfgResponse := BuildConfigResponse(getParam)
+
+		// mock
+		s.configRepo.EXPECT().GetConfigByConfigName(ctx, getParam).Return(cfgResponse, nil)
+		s.configRepo.EXPECT().RollbackConfigVersionByConfigName(ctx, gomock.AssignableToTypeOf(config)).Return(nil)
+
+		// When
+		resp, err := s.subject.RollbackConfigVersionByConfigName(ctx, config)
+
+		// Then
+		s.Nil(err, "Error should be nil")
+		s.NotNil(resp, "Response should in place")
+	})
+}
+
+func (s *configsUsecaseSuite) TestConfigs_RollbackConfig_Err() {
+	s.Run("Rollback Config - Err GetConfigByConfigName", func() {
+		// Given
+		ctx := context.TODO()
+		config := &entity.Config{
+			Name:    "test",
+			Version: 1,
+		}
+		getParam := &entity.GetConfigRequest{
+			Name: config.Name,
+		}
+		mockErr := testutil.ErrUnexpected
+
+		// mock
+		s.configRepo.EXPECT().GetConfigByConfigName(ctx, getParam).Return(nil, mockErr)
+
+		// When
+		resp, err := s.subject.RollbackConfigVersionByConfigName(ctx, config)
+
+		// Then
+		s.Nil(resp, "Response should be nil")
+		s.EqualError(mockErr, err.Error(), "Error should be equal")
+	})
+
+	s.Run("Rollback Config - Err Version is higher than lastest version", func() {
+		// Given
+		ctx := context.TODO()
+		config := &entity.Config{
+			Name:    "test",
+			Version: 3,
+		}
+		getParam := &entity.GetConfigRequest{
+			Name: config.Name,
+		}
+		cfgResponse := BuildConfigResponse(getParam)
+		expectedErr := entity.ErrConfigVersionNotFound(fmt.Sprint(config.Version))
+
+		// mock
+		s.configRepo.EXPECT().GetConfigByConfigName(ctx, getParam).Return(cfgResponse, nil)
+
+		// When
+		resp, err := s.subject.RollbackConfigVersionByConfigName(ctx, config)
+
+		// Then
+		s.Nil(resp, "Response should be nil")
+		s.EqualError(expectedErr, err.Error(), "Error should be equal")
+	})
+
+	s.Run("Rollback Config - Err Version is the same with lastest version", func() {
+		// Given
+		ctx := context.TODO()
+		config := &entity.Config{
+			Name:    "test",
+			Version: 2,
+		}
+		getParam := &entity.GetConfigRequest{
+			Name: config.Name,
+		}
+		cfgResponse := BuildConfigResponse(getParam)
+		expectedErr := entity.ErrRollbackNotAllowed
+
+		// mock
+		s.configRepo.EXPECT().GetConfigByConfigName(ctx, getParam).Return(cfgResponse, nil)
+
+		// When
+		resp, err := s.subject.RollbackConfigVersionByConfigName(ctx, config)
+
+		// Then
+		s.Nil(resp, "Response should be nil")
+		s.EqualError(expectedErr, err.Error(), "Error should be equal")
+	})
+
+	s.Run("Rollback Config - Err", func() {
+		// Given
+		ctx := context.TODO()
+		config := &entity.Config{
+			Name:    "test",
+			Version: 1,
+		}
+		getParam := &entity.GetConfigRequest{
+			Name: config.Name,
+		}
+		cfgResponse := BuildConfigResponse(getParam)
+		mockErr := testutil.ErrUnexpected
+
+		// mock
+		s.configRepo.EXPECT().GetConfigByConfigName(ctx, getParam).Return(cfgResponse, nil)
+		s.configRepo.EXPECT().RollbackConfigVersionByConfigName(ctx, gomock.Any()).Return(mockErr)
+
+		// When
+		resp, err := s.subject.RollbackConfigVersionByConfigName(ctx, config)
+
+		// Then
+		s.Nil(resp, "Response should be nil")
+		s.EqualError(mockErr, err.Error(), "Error should be equal")
+	})
+}
+
+func BuildConfigResponse(params *entity.GetConfigRequest) *entity.ConfigResponse {
+	return &entity.ConfigResponse{
+		Id:           5,
+		Name:         params.Name,
+		ConfigValues: "config",
+		Version:      2,
+		CreatedAt:    time.Now().UTC(),
+		ActorId:      1,
+	}
+}
