5acaa7fd8b538c3d4a06065069375b627bcf4fe1
bobyindra <bobyindra@outlook.com>
2025-11-18 19:31:13 +0700

    remove: unused parameter on config handler

--- module/configuration/internal/handler/configs_handler/config_handler_test.go
+++ module/configuration/internal/handler/configs_handler/config_handler_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/suite"
 
-	authMock "github.com/bobyindra/configs-management-service/module/configuration/internal/auth/mock"
 	configsHandler "github.com/bobyindra/configs-management-service/module/configuration/internal/handler/configs_handler"
 	usecaseMock "github.com/bobyindra/configs-management-service/module/configuration/internal/usecase/mock"
 	schemaMock "github.com/bobyindra/configs-management-service/module/configuration/schema/mock"
@@ -18,7 +17,6 @@ type configsHandlerSuite struct {
 	ctrl    *gomock.Controller
 	subject *configsHandler.ConfigsHandler
 
-	auth           *authMock.MockAuth
 	configsUsecase *usecaseMock.MockConfigsManagementUsecase
 	schemaRegistry *schemaMock.MockSchemaRegistry
 }
@@ -27,10 +25,9 @@ func (s *configsHandlerSuite) SetupTest() {
 	ctrl := gomock.NewController(s.T())
 
 	s.ctrl = ctrl
-	s.auth = authMock.NewMockAuth(ctrl)
 	s.configsUsecase = usecaseMock.NewMockConfigsManagementUsecase(ctrl)
 	s.schemaRegistry = schemaMock.NewMockSchemaRegistry(ctrl)
-	s.subject = configsHandler.NewConfigsHandler(s.auth, s.configsUsecase, s.schemaRegistry)
+	s.subject = configsHandler.NewConfigsHandler(s.configsUsecase, s.schemaRegistry)
 }
 
 func (s *configsHandlerSuite) TearDownTest() {
